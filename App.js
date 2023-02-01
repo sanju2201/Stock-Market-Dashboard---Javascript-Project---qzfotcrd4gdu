7fa13d8abae77a26626dd7e090421e9873621750
SanjuManna <sanjumanna2201@gmail.com>
2023-02-02 00:52:23 +0530

    Commit

--- App.js
+++ App.js
@@ -1,67 +1,23 @@
-/*
-API Parameters
-❚ Required: function
-
-The time series of your choice. In this case, function=TIME_SERIES_INTRADAY
-
-❚ Required: symbol
-
-The name of the equity of your choice. For example: symbol=IBM
-
-❚ Required: interval
-Time interval between two consecutive data points in the time series. The following values are supported: 1min, 5min, 15min, 30min, 60min
-
-❚ Optional: adjusted
-
-By default, adjusted=true and the output time series is adjusted by historical split and dividend events. Set adjusted=false to query raw (as-traded) intraday values.
-
-❚ Optional: outputsize
-
-By default, outputsize=compact. Strings compact and full are accepted with the following specifications: compact returns only the latest 100 data points in the intraday time series; full returns the full-length intraday time series. The "compact" option is recommended if you would like to reduce the data size of each API call.
-
-❚ Optional: datatype
-
-By default, datatype=json. Strings json and csv are accepted with the following specifications: json returns the intraday time series in JSON format; csv returns the time series as a CSV (comma separated value) file.
-
-❚ Required: apikey
-
-Your API key. Claim your free API key here.
-
-key ==> 3KSL9WN0OHTD9PZI
-Examples (click for JSON output)
-https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=IBM&interval=5min&apikey=demo */
 
 
-// fetch("https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=IBM&interval=5min&apikey=3KSL9WN0OHTD9PZI")
-// .then((data)=>{
-//     console.log(data);
-// })
-// .catch((srror)=>{
-//     console.log("Error");
-// });
-
-
-// fetch("https://jsonplaceholder.typicode.com/todos/")
-//   .then((res) => res.json())
-//   .then((todoItems) => {
-//     console.log(todoItems);
-//   })
-//   .catch((error) => {
-//     console.log("ERROR IN API CALL", error);
-//   });
+// key ==> 3KSL9WN0OHTD9PZI
+// Examples (click for JSON output)
+// https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=IBM&interval=5min&apikey=demo */
 
 
 // fetch("https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=AMZN&interval=5min&apikey=3KSL9WN0OHTD9PZI")
 //   .then((res) => res.json())
 //   .then((fetchedObj) => {
 //     console.log(fetchedObj);
+
+
 //   })
 //   .catch((error) => {
 //     console.log("ERROR IN API CALL", error);
 //   });
 
 
-  const fetchedObj ={
+  const localData ={
     "Meta Data": {
         "1. Information": "Intraday (5min) open, high, low, close prices and volume",
         "2. Symbol": "AMZN",
@@ -119,6 +75,8 @@ const daily = document.getElementById("daily");
 const weekly = document.getElementById("weekly");
 const monthly = document.getElementById("monthly");
 const listContainer = document.getElementById("watchlist-container");
+let closeButton = document.getElementById("close");
+
 
 // Watchlist to be stored in local Storage
 const myWatchlist = new Map();
@@ -170,25 +128,25 @@ type = "";
 searchButton.addEventListener("click", ()=>{
 let symbol = searchInput.value;
 let type = document.querySelector(".option-button.active").value;
-console.log(symbol)
-console.log(type)
+// console.log(symbol)
+// console.log(type)
 
 if(symbol && type){
 changeActiveItem();
 
 fetch(`https://www.alphavantage.co/query?function=TIME_SERIES_${type}&symbol=${symbol}&interval=5min&apikey=3KSL9WN0OHTD9PZI`)
   .then((res) => res.json())
-  .then((fetchedObj) => {
-    console.log("chlra ha")
-  
+  .then((fetchedObj) => {  
 let fetchSymbol = fetchedObj["Meta Data"]["2. Symbol"];
 let fetchType = fetchedObj["Meta Data"]["1. Information"].split(" ")[0];
 
-let output = fetchedObj["Time Series (5min)"];
+let mainKeys = Object.keys(fetchedObj);
+let output = fetchedObj[mainKeys[1]];
+
 let openPrice = Object.keys(output);
 let currentPrice = output[openPrice[0]]["1. open"];
 let oldPrice = output[openPrice[1]]["1. open"];
- 
+
 createNewListElement(fetchedObj, fetchSymbol, currentPrice, oldPrice, fetchType);
 searchInput.value ="";
 symbol= "";
@@ -207,31 +165,32 @@ console.log("then chlra ha")
 
 
 
-
 // Function to check for input and if not Present add to watchlist
 function createNewListElement(fetchedObj, fetchSymbol, currentPrice, oldPrice, fetchType){
-if (myWatchlist.has(`${fetchSymbol} ${fetchType}`)){
+if (myWatchlist.has(`${fetchSymbol}-${fetchType}`)){
     console.log("aleady present");
+    // let removeClass = listContainer.querySelector(`.${fetchSymbol}-${fetchType}`);
+    // removeClass.classList.remove("removed");
 }  else{
 let listItem = document.createElement("ul");
-listItem.setAttribute("class","watchlist");
+listItem.classList.add(`${fetchSymbol}-${fetchType}`)
+listItem.classList.add("watchlist");
+listItem.setAttribute("onclick","showDetails(event)");
 currentPrice = (Number(currentPrice)).toFixed(2);
 
-listItem.innerHTML = `<li id="symbol" class="list-element symbol">${fetchSymbol}</li>
-            <li id="price" class="list-element price">${currentPrice}</li>
-            <li id="information" class="list-element time">${fetchType.toUpperCase()}</li>
-            <li id="close" class="list-element close">
-            <i class="fa-solid fa-xmark"></i>
-            </li>`;
+listItem.innerHTML = `<li id="symbol" class="${fetchSymbol}-${fetchType} symbol">${fetchSymbol}</li>
+            <li  id="price" class="${fetchSymbol}-${fetchType} price">${currentPrice}</li>
+            <li  id="information" class="${fetchSymbol}-${fetchType} time">${fetchType.toUpperCase()}</li>
+            <li  id="close" class="${fetchSymbol}-${fetchType} close" onclick="closeElement(event)">
+            <i class="${fetchSymbol}-${fetchType} fa-solid fa-xmark"></i>
+           </li>`;
 
 listContainer.append(listItem);  
 // let priceCheck = document.getElementById("price");
+// console.log(listItem);
 
-let ele = document.querySelector(".watchlist:last-child");
-let priceCheck = ele.querySelector(".price");
-console.log(ele);
-
-console.log(priceCheck);
+let watchlist = document.querySelector(".watchlist:last-child");
+let priceCheck = watchlist.querySelector(".price");
 
 if(oldPrice > currentPrice){
     priceCheck.classList.add("bg-red");
@@ -240,10 +199,8 @@ if(oldPrice > currentPrice){
 } else {
      priceCheck.classList.add("bg-white");
 }
-console.log(currentPrice);
-console.log(oldPrice);
 
-myWatchlist.set(`${fetchSymbol} ${fetchType}`,getLastFiveDetails(fetchedObj));
+myWatchlist.set(`${fetchSymbol}-${fetchType}`,getLastFiveDetails(fetchedObj, fetchType));
 
 }
 console.log(myWatchlist);
@@ -251,18 +208,56 @@ console.log(myWatchlist);
 
 
 // Fetching last 5 details
-function getLastFiveDetails(fetchedObj){
-let output = fetchedObj["Time Series (5min)"];
+function getLastFiveDetails(fetchedObj ,fetchType){
+
+let mainKeys = Object.keys(fetchedObj);
+let output = fetchedObj[mainKeys[1]];
 let dayObject = Object.keys(output);
 
 let returnedMap = new Map();
 for(let i=0;i<5;i++){
-   returnedMap.set(dayObject[i].split(" ")[1], output[dayObject[i]]); 
+    if(fetchType === "Intraday"){
+    returnedMap.set(dayObject[i].split(" ")[1], output[dayObject[i]]); 
+    } else{
+    returnedMap.set(dayObject[i].split(" ")[0], output[dayObject[i]]); 
+    }
 }
-
 return returnedMap;
 }
 
+
+function closeElement(event){
+    event.stopPropagation();
+    
+    let clickedElement = event.target.classList[0];
+    let elementToBeRemoved = listContainer.querySelector(`.${clickedElement}`);
+    elementToBeRemoved.classList.add("removed");
+    
+    myWatchlist.delete(clickedElement);
+    
+
+
+
+    // clickedElement.classList.remove("");
+    console.log(myWatchlist)
+    // watchlist.classList.remove("removed");
+
+}
+
+
+function createModal(){
+    let divContainer = document.createElement("div");
+    divContainer.classList.add();
+}
+
+
+function showDetails(event){
+    // console.log("Clicked on Item");
+    let clickedElement = event.target.classList[0];
+    let result = myWatchlist.get(clickedElement);
+     console.log(result);
+
+}
 
 
 
@@ -300,3 +295,84 @@ entries()	Returns an iterator object with the [key, value] pairs in a Map
 keys()	Returns an iterator object with the keys in a Map
 values()	Returns an iterator object of the values in a Map
 */
+
+/*
+{
+    "Meta Data": {
+        "1. Information": "Monthly Prices (open, high, low, close) and Volumes",
+        "2. Symbol": "AMZN",
+        "3. Last Refreshed": "2023-01-31",
+        "4. Time Zone": "US/Eastern"
+    },
+    "Monthly Time Series": {
+        "2023-01-31": {
+            "1. open": "85.4600",
+            "2. high": "103.4850",
+            "3. low": "81.4300",
+            "4. close": "103.1300",
+            "5. volume": "1523798384"
+        }
+    }
+}*/ 
+
+/*
+{
+    "Meta Data": {
+        "1. Information": "Intraday (5min) open, high, low, close prices and volume",
+        "2. Symbol": "AMZN",
+        "3. Last Refreshed": "2023-01-31 20:00:00",
+        "4. Interval": "5min",
+        "5. Output Size": "Compact",
+        "6. Time Zone": "US/Eastern"
+    },
+    "Time Series (5min)": {
+        "2023-01-31 20:00:00": {
+            "1. open": "102.4200",
+            "2. high": "102.5000",
+            "3. low": "102.3900",
+            "4. close": "102.4100",
+            "5. volume": "23326"
+        }
+    }
+}*/ 
+
+/*{
+    "Meta Data": {
+        "1. Information": "Weekly Prices (open, high, low, close) and Volumes",
+        "2. Symbol": "AMZN",
+        "3. Last Refreshed": "2023-01-31",
+        "4. Time Zone": "US/Eastern"
+    },
+    "Weekly Time Series": {
+        "2023-01-31": {
+            "1. open": "101.0900",
+            "2. high": "103.3484",
+            "3. low": "99.0100",
+            "4. close": "103.1300",
+            "5. volume": "137219113"
+        }
+    }
+} */
+
+/*
+{
+    "Meta Data": {
+        "1. Information": "Daily Time Series with Splits and Dividend Events",
+        "2. Symbol": "AMZN",
+        "3. Last Refreshed": "2023-01-31",
+        "4. Output Size": "Compact",
+        "5. Time Zone": "US/Eastern"
+    },
+    "Time Series (Daily)": {
+        "2023-01-31": {
+            "1. open": "101.155",
+            "2. high": "103.3484",
+            "3. low": "101.14",
+            "4. close": "103.13",
+            "5. adjusted close": "103.13",
+            "6. volume": "66527253",
+            "7. dividend amount": "0.0000",
+            "8. split coefficient": "1.0"
+        }
+    }
+} */ 
